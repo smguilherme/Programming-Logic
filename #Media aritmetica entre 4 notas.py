8d4139cf838808aef4650ff13ee24911ccba0e52
Guilherme Eduardo Seixas Machado <75482125+smguilherme@users.noreply.github.com>
2025-01-20 21:59:01 -0300

    Create #Media aritmetica entre 4 notas.py

---
+++ #Media aritmetica entre 4 notas.py
@@ -0,0 +1,10 @@
+#Media aritmetica entre 4 notas
+
+n1=float(input('insira a nota do 1º bim: '))
+n2=float(input('insira a nota do 2º bim: '))
+n3=float(input('insira a nota do 3º bim: '))
+n4=float(input('insira a nota do 4º bim: '))
+
+ma=(n1+n2+n3+n4)/4
+
+print('A media aritmetica anual é: ',ma)
