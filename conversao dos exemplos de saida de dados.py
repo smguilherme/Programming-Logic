55f4ff17216ddec4af7e6077914b9a41e5f16aad
Guilherme Eduardo Seixas Machado <75482125+smguilherme@users.noreply.github.com>
2025-01-20 21:58:47 -0300

    Create conversao dos exemplos de saida de dados.py

---
+++ conversao dos exemplos de saida de dados.py
@@ -0,0 +1,17 @@
+# escreva (Y);
+print(Y)
+
+#escreva (B,xpto, Soma/4);
+print(B, xpto, soma/4)
+
+#escreva("bom dia", Nome);
+print("bom dia",nome)
+
+#escreva("voce pesa", p, "quilos")
+print("voce pesa", p, "quilos")
+
+#Metodo format relativo a classe str
+print('voce pesa {} quilos'.format(p)) #aqui p é exibido dentro do {}
+
+#outra forma é 
+print(f"voce {P}: quilos") #usando string literais formatadas (F'strings), inicia-se um string com f ou F antes de abrir aspas ou apostrofos
