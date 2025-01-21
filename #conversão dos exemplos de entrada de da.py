e6620ae117cc525e011105ea68bab5f985c528db
Guilherme Eduardo Seixas Machado <75482125+smguilherme@users.noreply.github.com>
2025-01-20 21:58:54 -0300

    Create #conversão dos exemplos de entrada de da.py

---
+++ #conversão dos exemplos de entrada de da.py
@@ -0,0 +1,12 @@
+#conversão dos exemplos de entrada de dados
+
+#leia(x);
+x = int(input('Informe o valor de x: '))
+
+#leia(y);
+y = float(input('Informa o valor de y: '))
+
+#leia(a, xpto, Nota);
+a = bool(input('Informe o valor de A'))
+xpto = input('Informe o valor de xpto: ')
+Nota = float(input( 'Informe o valor da nota: '))
